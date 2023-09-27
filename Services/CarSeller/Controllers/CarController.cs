b610acd53d95fc3b4a3af4f8f96b9fcddc97f627
Aditya Dixit <33696459+Adityageek@users.noreply.github.com>
2023-09-28 01:10:10 +0530

    new changes

--- Services/CarSeller/Controllers/CarController.cs
+++ Services/CarSeller/Controllers/CarController.cs
@@ -46,7 +46,7 @@ namespace CarSeller.Controllers
         }
 
         [HttpGet("{guid}")]
-        public async Task<ActionResult<CarDto>> GetCarByIdDuplicatessssssss(Guid guid)
+        public async Task<ActionResult<CarDto>> GetCarByIdDuplicatessssssssdddd(Guid guid)
         {
             var cars = await _carService.GetCarById(guid);
 
